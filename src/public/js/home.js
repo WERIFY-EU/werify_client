081018501d359df9ce90747fb6ff6e80a7ee9ac0
camiloramirez88 <camilo.ramirez@ivis.es>
2024-01-11 16:46:46 +0100

    add public folder

---
+++ src/public/js/home.js
@@ -0,0 +1,7 @@
+function redirectToLogin() {
+    window.location.href = 'https://staging.werify.eu/#/werify_point_kiosk/sultry+silicon+prance+designate+bonehead';
+}
+
+function redirectToRegistry() {
+    window.location.href = 'https://staging.werify.eu/#/werify_point_kiosk/traitor+tantrum+culinary+passport+humvee';
+}
